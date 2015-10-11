8e6232f60ed6e774e6f7d3721aaad248f5a009ef
Tadeu Sampaio <tadeubas@gmail.com>
2015-10-11 12:44:08 -0300

    Initial commit

---
+++ App_Code/agenda.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+
+
+public class agenda
+{
+    private int id;
+    private string nome;
+    private string telefone;
+
+    // Construtor padrão
+    public agenda() { }
+
+    // Construtor Overload
+    public agenda(int id, string nome, string telefone)
+    {
+        // Inicializa valores;
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    // Propriedades
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+        set
+        {
+            id = value;
+        }
+    }
+
+    public string Nome
+    {
+        get
+        {
+            return nome;
+        }
+
+        set
+        {
+            nome = value;
+        }
+    }
+
+    public string Telefone
+    {
+        get
+        {
+            return telefone;
+        }
+        set
+        {
+            telefone = value;
+        }
+    }
+
+}
---
+++ Scripts SQL/agenda.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE agenda
+(
+    [id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+    [nome] VARCHAR(100) NOT NULL, 
+    [telefone] VARCHAR(100) NOT NULL
+);
+
+INSERT INTO agenda (nome, telefone) VALUES ('Tadeu', '98124-7542');
